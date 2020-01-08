3f6d008edc7f9f0eb7e298948c284e8f91066929
Ryan Morrison <ryan.morrison@lytics.com>
2020-01-07 16:16:54 -0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,41 @@
-var gulp = require('gulp'),
-    less = require('gulp-less'),
-    cssmin = require('gulp-cssmin'),
-    plumber = require('gulp-plumber'),
-    rename = require('gulp-rename'),
-    connect = require('gulp-connect'),
-    del = require('del'),
-    destination = 'dist';
+var gulp = require("gulp"),
+  less = require("gulp-less"),
+  cssmin = require("gulp-cssmin"),
+  plumber = require("gulp-plumber"),
+  rename = require("gulp-rename"),
+  connect = require("gulp-connect"),
+  del = require("del"),
+  destination = "dist";
 
-gulp.task('html', function () {
-    gulp.src('./src/html/*')
-        .pipe(gulp.dest(destination))
+gulp.task("html", async function() {
+  gulp.src("./src/html/*").pipe(gulp.dest(destination));
 });
 
-gulp.task('js', function () {
-    gulp.src('./src/js/*')
-        .pipe(gulp.dest(destination))
+gulp.task("js", async function() {
+  gulp.src("./src/js/*").pipe(gulp.dest(destination));
 });
 
-gulp.task('img', function () {
-    gulp.src('./src/img/*')
-        .pipe(gulp.dest(destination + '/img/'))
+gulp.task("img", async function() {
+  gulp.src("./src/img/*").pipe(gulp.dest(destination + "/img/"));
 });
 
-gulp.task('clean', function () {
-  del([destination + '/**/*']);
+gulp.task("clean", async function() {
+  del.sync([destination + "/**/*"]);
 });
 
-gulp.task('less', function () {
-    gulp.src('./src/less/pathfora.less')
-        .pipe(plumber())
-        .pipe(less())
-        .pipe(rename("lytics_overrides.css"))
-        .pipe(gulp.dest(destination))
-        .pipe(cssmin())
-        .pipe(rename("lytics_overrides.min.css"))
-        .pipe(gulp.dest(destination))
-
+gulp.task("less", async function() {
+  gulp
+    .src("./src/less/pathfora.less")
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(rename("lytics_overrides.css"))
+    .pipe(gulp.dest(destination))
+    .pipe(cssmin())
+    .pipe(rename("lytics_overrides.min.css"))
+    .pipe(gulp.dest(destination));
 });
 
-gulp.task('preview', function () {
+gulp.task("preview", async function() {
   connect.server({
     port: 1234,
     root: destination,
@@ -46,14 +43,12 @@ gulp.task('preview', function () {
   });
 });
 
-gulp.task('watch', function () {
-    gulp.watch([
-        'src/less/**/*.less', 
-        'src/html/*.html',
-        'src/js/*.js',
-        'src/img/*'
-    ], ['build']);
+gulp.task("watch", async function() {
+  gulp.watch(
+    ["src/less/**/*.less", "src/html/*.html", "src/js/*.js", "src/img/*"],
+    gulp.series("build")
+  );
 });
 
-gulp.task('build', ['clean', 'html', 'js', 'img', 'less']);
-gulp.task('default', ['build', 'preview', 'watch']);
+gulp.task("build", gulp.parallel("clean", "html", "js", "img", "less"));
+gulp.task("default", gulp.series("build", "preview", "watch"));
